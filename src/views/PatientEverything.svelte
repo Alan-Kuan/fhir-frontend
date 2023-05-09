f3940160bc1453d22ab10c5a7e9f068e885eb900
Alan-Kuan <kuanhaoan@gmail.com>
2023-05-09 10:52:47 +0800

    feat: use select in PatientEverythingView

--- src/views/PatientEverything.svelte
+++ src/views/PatientEverything.svelte
@@ -1,49 +1,45 @@
 <main>
     <PageTitle>Patient Everything</PageTitle>
 
-    <form on:submit|preventDefault={onSubmit}>
-        <label class="input-group input-group-sm">
-            <span class="">Patient ID</span>
-            <input
-              class="input input-sm input-bordered"
-              type="text"
-              bind:value={patient_id}
-            />
-            <button
-              class="
-                btn btn-success btn-sm
-                text-white font-normal normal-case
-                hover:opacity-70"
-              type="submit"
-            >
-                <Icon
-                  icon="ic:baseline-search"
-                  class="text-lg"
-                />
-                Search
-            </button>
-        </label>
-    </form>
-
-    {#if ready}
-    <div class="mt-4">
-        <ResourceList get_resources={get_resources} />
-    </div>
+    {#if select_ready}
+    <Select
+        label="Patient"
+        bind:selected={selected_patient}
+        items={patient_select_items}
+    />
     {/if}
+
+    <div class="mt-4">
+        <ResourceList get_resources={getPatientEverything(selected_patient)} />
+    </div>
 </main>
 
 <script>
+    import { onMount } from 'svelte'
     import PageTitle from '$components/UI/PageTitle.svelte'
+    import Select from '$components/UI/Select.svelte'
     import ResourceList from '$components/ResourceList.svelte'
     import { getPatientEverything } from '$apis/Resource'
-    import Icon from '@iconify/svelte'
+    import { getResources } from '$apis/Resource'
 
-    let patient_id = ''
-    let ready = false
-    let get_resources
+    let select_ready = false
+    let patient_select_items
+    let selected_patient
 
-    function onSubmit() {
-        ready = true
-        get_resources = getPatientEverything(patient_id)
-    }
+    onMount(async () => {
+        const patients = await getResources('Patient')
+
+        patient_select_items = patients.map(patient => {
+            const id = patient.resource.id
+            const name = patient.resource.name
+            const display_name = name ? (name[0].given + ' ' + name[0].family) : 'Unknown'
+
+            return {
+                text: `${display_name} (${id})`,
+                value: id
+            }
+        })
+
+        select_ready = true
+    });
 </script>
